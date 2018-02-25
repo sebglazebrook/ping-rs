1c9465f36a9c62a26cf5fdd42a7810e62e10d95b
Seb Glazebrook <sebastian.glazebrook@redbubble.com>
2018-02-25 12:31:36 +1100

    Missing `mut` declaration

--- src/lib.rs
+++ src/lib.rs
@@ -6,7 +6,7 @@ use ping::{PingPacket, NetworkLayer};
 
 pub fn execute() {
     let packet = PingPacket::new();
-    let network_layer = NetworkLayer::new();
+    let mut network_layer = NetworkLayer::new();
     network_layer.send_packet(packet);
     let echo_response = network_layer.listen_for_echo_response();
     println!("{:?}", echo_response);
