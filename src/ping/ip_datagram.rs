863101022f45e15e98a7a9e7f73ce2532aecc3b0
Seb Glazebrook <sebastian.glazebrook@redbubble.com>
2018-02-26 17:27:41 +1100

    Moved IpHeader to it's own file

--- src/ping/ip_datagram.rs
+++ src/ping/ip_datagram.rs
@@ -1,6 +1,5 @@
-// TODO future -> handle things other than IPv4
+use ping::IpHeader;
 // First iteration, make one successful ping request with hardcoded destination and payload
-use byteorder::{WriteBytesExt, BigEndian};
 
 #[derive(Debug)]
 struct IcmpHeader {
@@ -45,83 +44,7 @@ impl IcmpPacket {
     pub fn into_bytes(&self) -> Vec<u8> {
         vec![] // TODO return actual bytes
     }
-
 }
-
-// TODO the IpHeader is big endian
-#[derive(Debug)]
-struct IpHeader {
-    // first 32-bits
-    version: u8, // TODO convert to 4 bits
-    header_length: u8, // TODO convert to 4 bits
-    type_of_service: u8, // 8 bits
-    total_length: u16, // 16 bits
-    // second 32-bits
-    identification: u16, // 16 bits
-    flags: u8, // need to end up being 3 bits
-    fragment_offset: u8, // 13 bits
-    // third 32-bits
-    time_to_live: u8, // 8 bits
-    protocol: u8, // 8 bits
-    header_checksum: u16, // 16 bits
-    // forth 32-bits
-    source_address: Vec<u8>, // 32 bits
-    // fifth 32-bits
-    destination_address: Vec<u8>, // 32 bits
-}
-
-impl IpHeader {
-
-    pub fn new() -> Self {
-        IpHeader {
-            version: 0x0800, // TODO correct value ??
-            header_length: 84, // TODO this needs to be calculated properly
-            type_of_service: 0x0,
-            total_length: 98, // TODO this needs to be calculated properly
-            identification: 1, // some number that increments for every datagram
-            flags: 0b000, // TODO find the real value for this
-            fragment_offset: 0, // TODO this needs to be dynamic, it starts at zero through
-            time_to_live: 64, // TODO this should be configurable
-            protocol: 1, // ICMP
-            header_checksum: 6969, // TODO calculate this properly
-            source_address: String::from("127.0.0.1").into_bytes(), // TODO this should be configurable (currently localhost)
-            destination_address: String::from("192.160.0.69").into_bytes(), // TODO this should be configurable (currently google)
-        }
-
-    }
-
-    pub fn into_bytes(&self) -> Vec<u8> {
-        let mut payload = vec![];
-        payload.push(self.version);
-        payload.push(self.header_length);
-        payload.push(self.type_of_service);
-
-        // Converting total_length from u16 -> Vec<u8>
-        let mut total_length: Vec<u8> = Vec::new();
-        total_length.write_u16::<BigEndian>(self.total_length).unwrap();
-        payload.extend(total_length);
-
-        // Converting identification from u16 -> Vec<u8>
-        let mut identification: Vec<u8> = Vec::new();
-        identification.write_u16::<BigEndian>(self.identification).unwrap();
-        payload.extend(identification);
-
-        payload.push(self.flags);
-        payload.push(self.fragment_offset);
-        payload.push(self.time_to_live);
-        payload.push(self.protocol);
-
-        // Converting header_checksum from u16 -> Vec<u8>
-        let mut header_checksum: Vec<u8> = Vec::new();
-        header_checksum.write_u16::<BigEndian>(self.header_checksum);
-        payload.extend(header_checksum);
-
-        payload.extend(self.source_address.clone());
-        payload.extend(self.destination_address.clone());
-        payload
-    }
-}
-
 
 #[derive(Debug)]
 pub struct IpDatagram {
--- src/ping/mod.rs
+++ src/ping/mod.rs
@@ -1,7 +1,9 @@
 mod ping_packet;
 mod ip_datagram;
+mod ip_header;
 mod network_layer;
 
 pub use self::ping_packet::PingPacket;
 pub use self::ip_datagram::IpDatagram;
+pub use self::ip_header::IpHeader;
 pub use self::network_layer::NetworkLayer;
